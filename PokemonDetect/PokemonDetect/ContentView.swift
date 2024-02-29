a205edb55ed25eaf1ba75fe64ec6c3cfebd993e2
Antonella Giugliano <giuglianoanton@gmail.com>
2024-02-29 12:35:32 +0100

    add camera view

--- PokemonDetect/PokemonDetect/ContentView.swift
+++ PokemonDetect/PokemonDetect/ContentView.swift
@@ -11,51 +11,65 @@ import PhotosUI
 struct ContentView: View {
     @StateObject var viewModel = ViewModel()
     @State private var selectedItem: PhotosPickerItem? = nil
-    @State private var selectedImageData: Data? = nil
-
+    @State private var selectedImage: UIImage? = nil
+    @State private var showCamera = false
+    
     @State var pokemon = ""
+    
     var body: some View {
         VStack {
-            if let selectedImageData,
-                       let uiImage = UIImage(data: selectedImageData) {
-                        Image(uiImage: uiImage)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 250, height: 250)
-                    }
+            if let uiImage = selectedImage {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+            }
             if let prediction = viewModel.prediction {
                 Text(pokemon)
             }
             Spacer()
-            PhotosPicker(
-                        selection: $selectedItem,
-                        matching: .images,
-                        photoLibrary: .shared()) {
-                            VStack{
-                                Image("pokeball").resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                Text("Select a pokemon")
-                            }
-                        }
-                        .onChange(of: selectedItem) { newItem in
-                            Task {
-                                viewModel.prediction = nil
-                                if let data = try? await newItem?.loadTransferable(type: Data.self) {
-                                    selectedImageData = data
-                                }
-                            }
-                        }
-            if let selectedImageData,
-               let uiImage = UIImage(data: selectedImageData), viewModel.prediction == nil {
+            VStack{
+                Image("pokeball").resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                
+                PhotosPicker(
+                    selection: $selectedItem,
+                    matching: .images,
+                    photoLibrary: .shared()) {
+                        
+                        Text("Select a pokemon")
+                    }
+                Button(action: {
+                    self.showCamera.toggle()
+                }, label: {
+                    Text("Camera")
+                })
+                .fullScreenCover(isPresented: self.$showCamera) {
+                    CameraView(selectedImage: self.$selectedImage)
+                }
+                
+            }
+            
+            .onChange(of: selectedItem) { newItem in
+                Task {
+                    viewModel.prediction = nil
+                    if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                        selectedImage = UIImage(data: data)
+                    }
+                }
+            }
+        
+            
+            if let uiImage = selectedImage, viewModel.prediction == nil {
                 Button(action: {
                     viewModel.classifyPokemon(image: uiImage)
                     pokemon = "\(viewModel.prediction!.classification) \(viewModel.prediction!.confidencePercentage)"
                 }, label: {
                     Text("Detect this Pokemon")
                 })
-                      
-                    }
+                
+            }
         }
         .padding()
         
